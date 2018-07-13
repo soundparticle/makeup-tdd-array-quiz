c1363a957999bec2a8648620c9a695f90a02c63b
Antreo Pukay <acrosswalker@yahoo.com>
2018-07-13 11:16:50 -0700

    filter more complete

---
+++ test/word.test.js
@@ -0,0 +1,14 @@
+const assert = require('assert');
+const { filter } = require('../lib/array');
+describe('array method', () => {
+
+    describe('filter words', () => {
+        it('removes words that contain y or Y', () => {
+            const words = ['sky', 'hot', 'Yeti', 'green'];
+            
+            const newArr = filter(words)
+            assert.deepEqual(newArr, ['Is it hot? Is it green?']);
+
+        });    
+    });
+});
